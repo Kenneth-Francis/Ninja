dcf7fcfcfa7072982667c11d8f10097c1a16c785
Kenneth Francis <kenneth.w.francis@outlook.com>
2023-08-28 13:05:52 -0400

    refactored code

--- ninja.js
+++ ninja.js
@@ -5,13 +5,23 @@ class Ninja {
     this.speed = 3;
     this.strength = 3;
   }
-  sayName = () => console.log(this.name);
-  showStats = () => console.log(`Name: ${this.name}\nHealth: ${this.health}\nSpeed: ${this.speed}\nStrength: ${this.strength}`);
-  drinkSake = () => this.health += 10;
+
+  sayName() {
+    console.log(this.name);
+    return this;
+  }
+
+  showStats() {
+    console.log(`Name: ${this.name}\nHealth: ${this.health}\nSpeed: ${this.speed}\nStrength: ${this.strength}`);
+    return this;
+  }
+
+  drinkSake() {
+    console.log(`${this.name} drank sake. Health bolstered`);
+    this.health += 10;
+    return this;
+  }
 }
 
 const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();     // logs name of ninja
-ninja1.showStats();   // shows all properties and values of ninja
-ninja1.drinkSake();   // adds 10 health to ninja
-ninja1.showStats();   // shows all properties and values of ninja
+ninja1.sayName().showStats().drinkSake().showStats();
